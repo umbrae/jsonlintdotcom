ae45d4226db6c40c051438f3bea5e961218911e1
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-08-05 15:22:18 +0300

    switched bundler to webpack, made needed clean ups

---
+++ webpack.config.babel.js
@@ -0,0 +1,51 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import OpenBrowserPlugin from 'open-browser-webpack-plugin';
+import path from 'path';
+
+const { NODE_ENV } = process.env;
+
+const entry = [];
+const plugins = [];
+
+if (NODE_ENV === 'development') {
+    entry.push('webpack-dev-server/client?http://localhost:8101');
+    plugins.push(new OpenBrowserPlugin({
+        url: 'http://localhost:8101',
+        ignoreErrors: true
+    }));
+}
+
+entry.push('./js/app/app.js');
+plugins.push(new CopyWebpackPlugin([
+	{ from: 'css', to: 'css' },
+    { from: 'img', to: 'img' },
+	{ from: 'index.html' },
+    { from: 'proxy.php' },
+    { from: 'buysellads.php' },
+    { from: 'include.php' },
+    { from: 'favicon.ico' }
+]));
+
+module.exports = {
+    entry,
+    plugins,
+    context: __dirname,
+    output: {
+        path: path.resolve('dist/'),
+        filename: 'js/app.js',
+        library: 'app',
+        libraryTarget: 'var'
+    },
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loaders: ['babel'],
+            include: path.resolve('js/')
+        }]
+    },
+    devtool: 'source-map',
+    eslint: {
+        configFile: '.eslintrc.json'
+    }
+};
