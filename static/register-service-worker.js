ed7940f98f97209568cf8b13f399b86bcd97846a
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-24 13:33:18 +0300

    fix lint errors

--- static/register-service-worker.js
+++ static/register-service-worker.js
@@ -1,5 +1,8 @@
-navigator.serviceWorker.register('service-worker.js').then(function() {
-    console.log('CLIENT: service worker registration complete.');
-}, function(e) {
-    console.error(e);
-});
+/* eslint-disable prefer-arrow-callback, no-console */
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('service-worker.js').then(function done() {
+        console.log('CLIENT: service worker registration complete.');
+    }, function c(e) {
+        console.error(e);
+    });
+}
